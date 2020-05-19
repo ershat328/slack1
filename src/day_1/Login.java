67f27989859263c66b28bfc33184ce9e9317c920
ershat328 <57468265+ershat328@users.noreply.github.com>
2020-05-19 04:48:38 +0200

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -3,6 +3,9 @@ package day_1;
 public class Login {
     public static void main(String[] args) {
         //slack users can login
-        System.out.println ("hello login" );
+        System.out.println ("hello login")
+                    System.out.println ("bye login")
+
+                            
     }
 }
